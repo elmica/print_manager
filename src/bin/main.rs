e4a8b1a801a7bdbb2c9e3a4e72f1b91f8bd42dec
elmica <mb.codingtest@gmail.com>
2024-12-28 22:40:15 -0500

    second

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,23 +4,22 @@ use std::fs;
 
 
 fn main(){
-    print!("Test main")
     match print_manager::get_all_printers() {
         Ok(json_data) => println!("{:?}", serde_json::to_string(&json_data).unwrap()),
         Err(err) => println!("{:?}", err), 
     }
     
-    // match print_manager::print_file("00057.SPL", "receipt") {
-    //     Ok(_) => println!("Printout successful"),
-    //     Err(e) => println!("Error: {}", e),
-    // }
+    match print_manager::print_file("00057.SPL", "receipt") {
+        Ok(_) => println!("Printout successful"),
+        Err(e) => println!("Error: {}", e),
+    }
 
-    // let file_path = "00057.SPL";
+    let file_path = "00057.SPL";
 
-    // let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    // match print_manager::print_data(&contents, "receipt") {
-    //     Ok(_) => println!("Printout successful"),
-    //     Err(e) => println!("Error: {}", e),
-    // }
+    match print_manager::print_data(&contents, "receipt") {
+        Ok(_) => println!("Printout successful"),
+        Err(e) => println!("Error: {}", e),
+    }
 }
